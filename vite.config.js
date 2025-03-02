e60753e5e744be8f28a07e436001ca9684a12c88
Shanmukha Pothukuchi <shanmukha.pothukuchi@gmail.com>
2025-03-02 16:45:22 -0500

    rebrand and update spelling errors, add default fields for dropdown in forms

--- vite.config.js
+++ vite.config.js
@@ -25,9 +25,9 @@ export default defineConfig({
     VitePWA({
       includeAssets: ["assets/*", "icons/*"],
       manifest: {
-        name: "Black Hawks Scouting",
+        name: "Bovine Intervention Scouting",
         short_name: "Scouting",
-        description: "A scouting app by FRC Team 2834",
+        description: "A scouting app by FRC Team 686",
         theme_color: "#292929",
         background_color: "#292929",
         icons: [
